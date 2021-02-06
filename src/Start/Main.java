e2d0490f6d6969b3407588e7678f2e814c391f63
Andrei Kojemiakin <kav231@yandex.ru>
2021-02-06 08:10:56 +0300

    Final cod

--- src/Start/Main.java
+++ src/Start/Main.java
@@ -1,16 +1,19 @@
 package Start;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+
 public class Main {
-
-    public static void main(String[] args) {
-
-       int count = 0;
-      do {
-          ++count;
-          System.out.println(count);
-      } while (count < 5);
-
-
-       }
+    public static void main(String[] args){
+        System.out.println(calcul("rrettyuiytii"));
     }
+    static HashMap<Character, Boolean> calcul(@NotNull String str){
+        HashMap<Character, Boolean> map = new HashMap<>();
+        for (char ch : str.toCharArray() ){
+            map.put(ch,map.containsKey(ch));
+        }
+        return map;
+    }
+}
 
